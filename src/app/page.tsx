daeeb9321f1032d1f3e23c8fba97dd0cd9a7d1df
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2025-01-17 22:15:46 +0100

    added text to home page styled this text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import Header from '@/app/components/Header';
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Header />
-    </>
+      <h1 className="px-6 text-white pt-20 text-2xl font-bold">
+        Welcome to home page
+      </h1>
+    </div>
   );
 }
